ec07eb47cba8233b758dc19da5e8292a9eedb15c
Enkhbayarrrrrrr <eegy66410@gmail.com>
2025-01-29 14:08:55 +0800

    h

---
+++ lab21/eegy2.py
@@ -0,0 +1,7 @@
+course_id=input("хичээлийн код:")
+course_name=input("хичээлийн нэр:")
+credit=input("хичээлийн кредит:")
+madlib="энэ хичэлийн код нь {}".format(course_id).upper()+\
+",хичээлийн нэр:{}".format(course_name).title()+\
+"бөгөөд уг хичээл нь {}".format(credit)+ "кредитийн хичээл юм."
+print(madlib)
